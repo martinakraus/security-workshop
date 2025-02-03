e61cf4d52f20ebc81c198892889a499a69c9a577
Martina Kraus <kraus.martina.m@gmail.com>
2025-02-03 15:02:38 +0100

    update projects and demos

---
+++ cors-app/cors-server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+ app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: 'POST',
+  allowedHeaders: 'Content-Type, X-Custom-Header', 
+  credentials: true
+ }));
+
+const port = 4000;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// GET request endpoint
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'This is a GET request' });
+});
+
+// POST request endpoint
+app.post('/api/login', (req, res) => {
+  const receivedData = req.body;
+  res.json({ message: 'This is a POST request', data: receivedData });
+});
+
+app.put('/api/login', (req, res) => {
+  const receivedData = req.body;
+  res.json({ message: 'This is a PUT request', data: receivedData });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
